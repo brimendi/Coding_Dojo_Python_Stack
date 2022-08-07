06da0a8058c815513495bdef3a48cb9c96de0624
Brignie Mendieta <brigniem@gmail.com>
2022-08-07 14:05:00 -0700

    changes

--- Flask/hello_flask/server.py
+++ Flask/hello_flask/server.py
@@ -5,8 +5,6 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def hello_world():
     return 'Hello World!!'  # Return the string 'Hello World!' as a response
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode.
 
 # import statements, maybe some other routes
     
@@ -16,3 +14,5 @@ def success():
     
     # app.run(debug=True) should be the very last statement! 
 
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
