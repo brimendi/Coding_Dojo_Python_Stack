a9d2b01a4f15286f30ca6c3bddd63f61cf4d3556
Brignie Mendieta <brigniem@gmail.com>
2022-08-13 17:20:53 -0700

    updates

--- Flask+MySQL/User_CR/server.py
+++ Flask+MySQL/User_CR/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from users_model import Users
+from models.users_model import Users
 app = Flask(__name__)                     
 
 # READ ALL ROUTE
@@ -8,15 +8,53 @@ def get_all():
     all_users = Users.get_all()
     return render_template('read_all.html', users = all_users)
 
-# INPUT NEW USER INFO
+# READ ONE USERS INFO
+@app.route("/user/<int:id>")
+def display_one(id):
+    data = {
+        "id": id
+    }
+    user = Users.get_one(data)
+    return render_template("read_one.html", user = user)
+
+# CREATE NEW USER
 @app.route('/user/new')
 def new_user_form():
     return render_template('create.html')
 
-# CREATE NEW USER
+
 @app.route('/user/create', methods=['POST'])
 def create_user():
-    Users.create_user(request.form)
+    id = Users.create_user(request.form)
+    return redirect(f'/user/{id}')
+
+# UPDATE A USER INFO 
+@app.route('/user/<int:id>/edit')
+def edit_user(id):
+    data = {
+        "id" : id
+    }
+    user = Users.get_one(data)
+    return render_template("edit.html", user = user)
+
+@app.route('/user/<int:id>/update', methods = ["POST"])
+def update_user(id):
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "id": id
+    }
+    Users.update(data)
+    return redirect("/")
+
+# DELETE A USER 
+@app.route('/user/<int:id>/delete')
+def delete(id):
+    data = {
+        "id" : id
+    }
+    Users.delete(data)
     return redirect('/')
 
 if __name__=="__main__":
